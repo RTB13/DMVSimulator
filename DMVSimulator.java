cccb514505ee35dcf52530ecdc94f2ac4397fc8b
RTB13 <rtberrigan@gmail.com>
2025-03-05 16:07:17 -0500

    DMV Simulator

--- DMVSimulator.java
+++ DMVSimulator.java
@@ -1,4 +1,21 @@
-
 public class DMVSimulator {
+	public static void main (String[] args) {
+		System.out.println("Welcome to the DMV!");
+		
+		int number = (int) (Math.random() * 100) + 1;
+        System.out.println("Your number is: " + number + ". Please wait until your number is called.");
 
+		
+		for (int i = number + 1; i <= 100; i++ ) {
+			System.out.println("Now Serving: " + i);
+		}
+		for (int i = 1; i < number; i++) {
+            System.out.println("Now serving: " + i);
+        }
+		
+
+		System.out.println();
+        System.out.println("Sorry, you don’t have the required paperwork.");
+
+	}
 }
